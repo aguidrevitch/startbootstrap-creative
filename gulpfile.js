6b026890f20fc0e14e8bb04880cc2f47fc7e55fa
Aleksandr Guidrevitch <aguidrevitch@darwinapps.com>
2020-05-30 16:49:33 +0300

    new site

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,109 @@
 "use strict";
 
 // Load plugins
-const autoprefixer = require("gulp-autoprefixer");
+const path = require('path');
 const browsersync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const del = require("del");
 const gulp = require("gulp");
-const header = require("gulp-header");
-const merge = require("merge-stream");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const inlinesource = require('gulp-inline-source');
 
 // Load package.json for banner
 const pkg = require('./package.json');
 
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
 
 // BrowserSync
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000,
-    open: false
-  });
-  done();
+    browsersync.init({
+        server: {
+            baseDir: "./",
+            reloadDebounce: 500
+        },
+        port: 3000,
+        open: false
+    });
+    done();
 }
 
 // BrowserSync reload
 function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+    browsersync.reload();
+    done();
 }
 
 // Clean vendor
 function clean() {
-  return del(["./vendor/"]);
-}
-
-// Bring third party dependencies from node_modules into vendor directory
-function vendor() {
-    var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
-        .pipe(gulp.dest('./vendor'));
-
-    return merge(fontAwesome);
+    return del(["./*.html", "./css/*", "./js/*"]);
 }
 
 // CSS task
 function css() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: "expanded",
-      includePaths: "./node_modules",
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest("./css"))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
-    .pipe(browsersync.stream());
+    return gulp
+        .src([ 
+            "./src/scss/*.scss", 
+            "!./src/scss/_*.scss"
+        ])
+        .pipe(plumber())
+        .pipe(sass({
+            // outputStyle: "expanded",
+            includePaths: "./node_modules",
+        }))
+        .on("error", sass.logError)
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("./css"))
+        .pipe(browsersync.stream());
 }
 
 // JS task
 function js() {
-  return gulp
-    .src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browsersync.stream());
+    return gulp
+        .src([
+            './node_modules/plyr/dist/plyr.js',
+            './src/js/*.js',
+        ])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./js'))
+        .pipe(browsersync.stream());
 }
+
+function inline() {
+    return gulp.src('./src/html/*.html')
+        .pipe(inlinesource({compress: false}))
+        .pipe(rename(function (file) {
+            file.dirname = path.dirname(file.dirname);
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(browsersync.stream());
+};
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./scss/**/*", css);
-  gulp.watch(["./js/**/*.js", "!./js/**/*.min.js"], js);
-  gulp.watch("./**/*.html", browserSyncReload);
+    gulp.watch(["./src/scss/**/*"], gulp.series(css, inline, browserSyncReload));
+    gulp.watch(["./src/js/**/*"], gulp.series(js, inline, browserSyncReload));
+    gulp.watch(["./src/html/**/*.html"], gulp.series(inline, browserSyncReload));
 }
 
 // Define complex tasks
-const build = gulp.series(clean, gulp.parallel(css, js, vendor));
+const build = gulp.series(clean, gulp.parallel(css, js), inline);
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
 exports.js = js;
 exports.clean = clean;
+exports.inline = inline;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
---
+++ src/js/index.js
@@ -0,0 +1,105 @@
+(function (window, document) {
+    'use strict'; // Start of use strict
+
+    var form = document.querySelector('form');
+    var mcMap = {
+        'utm_campaign': 'MMERGE6',
+        'utm_source': 'MMERGE7',
+        'utm_medium': 'MMERGE8',
+        'utm_content': 'MMERGE9',
+    };
+    if (URLSearchParams) {
+
+        (new URLSearchParams(window.location.search)).forEach(function (value, name) {
+            if (mcMap[name]) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'hidden');
+                input.setAttribute('name', mcMap[name]);
+                input.setAttribute('value', value);
+                form.appendChild(input);                
+            }
+        });
+    }
+  
+    window.subscribed = function (result) {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({ 'event': 'formSubmit' });
+        window.dataLayer.push({ 'event': 
+            'success' == (result.result || '') 
+                ? 'formSubmitSuccess' 
+                : 'formSubmitError'
+        });
+        
+        var cta = document.querySelector('#cta');
+        var thankYou = document.querySelector('#thank-you');
+        form.classList.add('fade-out');
+        cta.classList.add('fade-out');
+        setTimeout(function () {
+            cta.style.display = 'none';
+            thankYou.style.display = 'block';
+            thankYou.classList.add('fade-in');
+        }, 500);
+    };
+
+    var formSubmit = function (e) {
+        e.preventDefault();
+
+        var action = form.getAttribute('action').replace('/post?', '/post-json?');
+        var data = new URLSearchParams();
+        (new FormData(form)).forEach(function (value, field) {
+            data.append(field, value);
+        });
+        data.append('c', 'subscribed');
+
+        var head= document.getElementsByTagName('head')[0];
+        var script= document.createElement('script');
+        script.type= 'text/javascript';
+        script.src= action + '&' + data;
+        head.appendChild(script);
+
+        form.removeEventListener('submit', formSubmit);
+    };
+
+    document.querySelectorAll('[data-start]').forEach(function (el) {
+        var start = parseInt(el.dataset.start);
+        setTimeout(function () {
+            //console.log("start", start);
+            el.classList.add('fade-in');
+            if (el.nodeName == 'VIDEO') {
+                el.play();
+            }
+        }, start);
+    });
+
+    document.querySelectorAll('[data-stop]').forEach(function (el) {
+        var stop = parseInt(el.dataset.stop);
+        setTimeout(function () {
+            //console.log("stop", stop);
+            if (el.nodeName == 'VIDEO') {
+                setTimeout(function () {
+                    el.parentNode.removeChild(el);
+                }, 1000);
+            } else {
+                el.classList.add('fade-out');
+            }
+        }, stop);
+    });
+
+    form.addEventListener('submit', formSubmit);
+    window.scrollTo(0,1);
+
+    var player = new Plyr('video');
+    document.querySelector('.playbutton').addEventListener('click', function () {
+        document.querySelector('body').classList.add('modal-open');
+        document.querySelector('.modal').style.display = 'flex';
+        player.play();
+        // player.fullscreen.enter();
+    });
+
+    document.querySelector('.close').addEventListener('click', function () {
+        player.stop();
+        document.querySelector('.modal').style.display = 'none';
+        document.querySelector('body').classList.remove('modal-open');
+    });
+
+})(window, document); // End of use strict
